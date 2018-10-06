e53ee292131d51706f7ee6904296642ab51d0038
hhwe <hhw12125@gmail.com>
2018-10-06 12:09:23 +0800

    go's net/http module build web app

--- app.go
+++ app.go
@@ -1,31 +1,16 @@
 package main
 
 import (
-	"webgo/handles"
-
-	"github.com/gin-gonic/gin"
+	"fmt"
+	"net/http"
+	"time"
 )
 
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World! %s", time.Now())
+}
+
 func main() {
-	router := gin.Default()
-
-	router.GET("/", handles.HomePage)
-	router.POST("/login", handles.Login)
-
-	v1 := router.Group("api/v1")
-	{
-		v1.GET("/users", handles.AllUsers)
-		v1.GET("/users/:id", handles.FindUser)
-		v1.POST("/users", handles.CreateUser)
-		v1.PATCH("/users", handles.UpdateUser)
-		v1.DELETE("/users", handles.DeleteUser)
-
-		v1.GET("/books", handles.AllBooks)
-		v1.GET("/books/:id", handles.FindBook)
-		v1.POST("/books", handles.CreateBook)
-		v1.PATCH("/books", handles.UpdateBook)
-		v1.DELETE("/books", handles.DeleteBook)
-	}
-
-	router.Run()
+	http.HandleFunc("/", greet)
+	http.ListenAndServe(":8080", nil)
 }
--- handles/handles.go
+++
@@ -1,7 +0,0 @@
-package handles
-
-import "github.com/gin-gonic/gin"
-
-func HomePage(c *gin.Context) {
-	c.String(200, "home page")
-}
