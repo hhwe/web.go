d5dcf8a45ecf0a653f8e8238ddf4a996ebafab27
hhwe <hhw12125@gmail.com>
2018-11-05 22:02:52 +0800

    make application context structor

--- context.go
+++ context.go
@@ -4,7 +4,9 @@ package webgo
 import (
 	"context"
 	"log"
+	"mime"
 	"net/http"
+	"strings"
 )
 
 func Authentication(next http.Handler) http.Handler {
@@ -24,7 +26,92 @@ func Authentication(next http.Handler) http.Handler {
 // Context is the most important part of webgo. It allows us to pass variables between middleware,
 // manage the flow, validate the JSON of a request and render a JSON response for example.
 type Context struct {
-	Request *http.Request
-	Writer  http.ResponseWriter
-	Params  []string
+	Request     *http.Request
+	Params      map[string]string
+	Application *App
+	http.ResponseWriter
+}
+
+// WriteString writes string data into the response object.
+func (ctx *Context) WriteString(content string) {
+	ctx.ResponseWriter.Write([]byte(content))
+}
+
+// Abort is a helper method that sends an HTTP header and an optional
+// body. It is useful for returning 4xx or 5xx errors.
+// Once it has been called, any return value from the handler will
+// not be written to the response.
+func (ctx *Context) Abort(status int, body string) {
+	ctx.SetHeader("Content-Type", "text/html; charset=utf-8", true)
+	ctx.ResponseWriter.WriteHeader(status)
+	ctx.ResponseWriter.Write([]byte(body))
+}
+
+// Redirect is a helper method for 3xx redirects.
+func (ctx *Context) Redirect(status int, url_ string) {
+	ctx.ResponseWriter.Header().Set("Location", url_)
+	ctx.ResponseWriter.WriteHeader(status)
+	ctx.ResponseWriter.Write([]byte("Redirecting to: " + url_))
+}
+
+//BadRequest writes a 400 HTTP response
+func (ctx *Context) BadRequest() {
+	ctx.ResponseWriter.WriteHeader(400)
+}
+
+// Notmodified writes a 304 HTTP response
+func (ctx *Context) NotModified() {
+	ctx.ResponseWriter.WriteHeader(304)
+}
+
+//Unauthorized writes a 401 HTTP response
+func (ctx *Context) Unauthorized() {
+	ctx.ResponseWriter.WriteHeader(401)
+}
+
+//Forbidden writes a 403 HTTP response
+func (ctx *Context) Forbidden() {
+	ctx.ResponseWriter.WriteHeader(403)
+}
+
+// NotFound writes a 404 HTTP response
+func (ctx *Context) NotFound(message string) {
+	ctx.ResponseWriter.WriteHeader(404)
+	ctx.ResponseWriter.Write([]byte(message))
+}
+
+// ContentType sets the Content-Type header for an HTTP response.
+// For example, ctx.ContentType("json") sets the content-type to "application/json"
+// If the supplied value contains a slash (/) it is set as the Content-Type
+// verbatim. The return value is the content type as it was
+// set, or an empty string if none was found.
+func (ctx *Context) ContentType(val string) string {
+	var ctype string
+	if strings.ContainsRune(val, '/') {
+		ctype = val
+	} else {
+		if !strings.HasPrefix(val, ".") {
+			val = "." + val
+		}
+		ctype = mime.TypeByExtension(val)
+	}
+	if ctype != "" {
+		ctx.Header().Set("Content-Type", ctype)
+	}
+	return ctype
+}
+
+// SetHeader sets a response header. If `unique` is true, the current value
+// of that header will be overwritten . If false, it will be appended.
+func (ctx *Context) SetHeader(hdr string, val string, unique bool) {
+	if unique {
+		ctx.Header().Set(hdr, val)
+	} else {
+		ctx.Header().Add(hdr, val)
+	}
+}
+
+// SetCookie adds a cookie header to the response.
+func (ctx *Context) SetCookie(cookie *http.Cookie) {
+	ctx.SetHeader("Set-Cookie", cookie.String(), false)
 }
